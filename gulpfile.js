dab0686326d0ded81900ff33f61b0a68ca673fd2
justinsa <justinsa@live.ca>
2015-07-20 03:12:25 -0700

    Clean-up of build and test files
    * Moved "spec\services.js" to "tests.js"
    * Updated to latest versions of all dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 gulp.task('js:lint', function() {
-  return gulp.src(['./angular-local-storage-service.js', './gulpfile.js', './spec/services.js'])
+  return gulp.src(['./angular-local-storage-service.js', './gulpfile.js', './tests.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 gulp.task('js:test', function() {
-  var mochaPhantom = require('gulp-mocha-phantomjs');
+  var mochaPhantomJS = require('gulp-mocha-phantomjs');
   return gulp.src('./test.html')
-    .pipe(mochaPhantom());
+    .pipe(mochaPhantomJS());
 });
 gulp.task('default', ['js:lint', 'js:test']);
